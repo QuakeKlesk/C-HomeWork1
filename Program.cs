2d95443819d1eda0fcee3a2cda78a1456034cb62
Sergey.Konovalov <konovalovs777@mail.ru>
2022-08-17 20:41:20 +0700

    Work is done

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
-﻿// ЗАДАЧА 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+﻿/* // ЗАДАЧА 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
-/*Console.WriteLine("Введите первое число для сравнения");
+Console.WriteLine("Введите первое число для сравнения");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число для сравнения");
 int number2 = Convert.ToInt32(Console.ReadLine());
@@ -13,10 +13,9 @@ else
     Console.WriteLine(number2 + " больше чем " + number1);
 }*/
 
+/* // ЗАДАЧА 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-// ЗАДАЧА 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-
-/*Console.WriteLine("Введите три числа для сравнения");
+Console.WriteLine("Введите три числа для сравнения");
 int number1 = Convert.ToInt32(Console.ReadLine());
 int number2 = Convert.ToInt32(Console.ReadLine());
 int number3 = Convert.ToInt32(Console.ReadLine());
@@ -31,9 +30,9 @@ if (number3 > result)
 }
 Console.WriteLine(result);*/
 
-// ЗАДАЧА 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+/* // ЗАДАЧА 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
-/*Console.WriteLine("Введите число");
+Console.WriteLine("Введите число");
 int number = Convert.ToInt32(Console.ReadLine());
 if (number % 2 == 0)
 {
@@ -44,16 +43,16 @@ else
     Console.WriteLine("нет");
 }*/
 
-
 // ЗАДАЧА 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
 Console.WriteLine("Введите число");
-int number = Convert.ToInt32(Console.ReadLine());
+int num = Convert.ToInt32(Console.ReadLine());
 int index = 0;
-while (index <= nember)
+while (index <= num)
 {
-    if (index % 2 ==0)
+    if (index % 2 == 0)
     {
-        
+        Console.Write(index + ", "); 
     }
+    index++;
 }
