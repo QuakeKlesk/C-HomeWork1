e226e1e7fff46c4da98fecd0304edb07d0d9a50d
Sergey.Konovalov <konovalovs777@mail.ru>
2022-08-18 18:09:08 +0700

    Task 19 done

--- Program.cs
+++ Program.cs
@@ -64,12 +64,44 @@ Console.WriteLine("Введите трехзначное число");
 string num = Console.ReadLine();
 Console.WriteLine(num[1]);*/
 
+/* // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-Console.WriteLine("Введите трехзначное число");
+Console.WriteLine("Введите число");
 string num = Console.ReadLine();
-Console.WriteLine(num[1]);
-if (num == void)
+int length = num.Length;
+if (length <= 2)
 {
     Console.WriteLine("цифры нет");
 }
+else Console.WriteLine(num[2]); */
 
+/* // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите число от 1 до 7");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num == 1) Console.Write("Понедельник");
+if (num == 2) Console.Write("Вторник");
+if (num == 3) Console.Write("Среда");
+if (num == 4) Console.Write("Четверг");
+if (num == 5) Console.Write("Пятница");
+if (num == 6) Console.Write("Суббота");
+if (num == 7) Console.Write("Воскресенье");
+if (num <=5) Console.Write(", работаем :( ");
+if (num >=6 ) Console.Write(", отдыхаем :) ");
+*/
+
+/* // Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число");
+string num = Console.ReadLine();
+if (num[0] == num[4])
+{
+    if (num[1] == num[3]) Console.WriteLine("да");
+}
+else Console.WriteLine("нет");*/
+
+// Задача 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+
+
+// Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
